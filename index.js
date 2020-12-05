03e1ff57b042154099f2e8754ad1a6b621a21a11
mamooredesigns <mamooredesigns@gmail.com>
2020-12-04 21:40:27 -0500

    updating

--- index.js
+++ index.js
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-/* Challenge #1
- * Create a function that accepts two number arguments and returns the product
- *
- * Steps:
- * 1. Create a function called `multiply`
- * 2. Add two parameters in your function signature, `num1` and `num2`
- * 3. Write logic to find the product of `num1` and `num2`, but you must do so without using the `*` operator
- * 4. Add a return statement in your function which returns the `product`
- * 7. Use `console.log` to print out the output of `multiply()`
- *
- * Hint: multiplication is a sequence of sums (2 * 3 = 2 + 2 + 2)
- */
-
-// TODO: ADD CODE HERE
-
-/* Challenge #2
- * ...
- *
- * Steps:
- * 1. ...
- *
- * Hint: ...
- */
-
-// TODO: ADD CODE HERE
-
-/* Challenge #3
- * ...
- *
- * Steps:
- * 1. ...
- *
- * Hint: ...
- */
-
-// TODO: ADD CODE HERE
-
-/* Challenge #4
- * ...
- *
- * Steps:
- * 1. ...
- *
- * Hint: ...
- */
-
-const library = [
-  {
-    title: 'The Road Ahead',
-    author: 'Bill Gates',
-    libraryID: 1254,
-  },
-  {
-    title: 'Walter Isaacson',
-    author: 'Steve Jobs',
-    libraryID: 4264,
-  },
-  {
-    title: 'Mockingjay: The Final Book of The Hunger Games',
-    author: 'Suzanne Collins',
-    libraryID: 3245,
-  },
-]
-
-// TODO: ADD CODE HERE
-=======
 
 //------------------------ Question 1---------------------------
 //Write a function that ask the user for two numbers and return the product of them. But the challenge here is that you can't use the operator * (you can't make the product)
@@ -218,4 +151,3 @@ const answer4 = sort()
 
 const htmlTarget4 = document.getElementById('a-4')
 htmlTarget4.innerHTML = answer4
->>>>>>> master
